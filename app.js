d3e0d42c8ac855a1d8ab2eca5cf2c3c017375380
gustavsant <gustavosant3301@gmail.com>
2022-09-14 05:38:23 -0300

    m

---
+++ app.js
@@ -0,0 +1,34 @@
+class Person {
+  constructor(classs, str, dex, int) {
+    this.classs = classs
+    this.str = str
+    this.dex = dex
+    this.int = int
+  }
+}
+class Human extends Person {
+  constructor(classs, str, dex, int) {
+    super(classs, str * 1.6, dex * 1.6, int * 1.6)
+  }
+}
+
+class Elf extends Person {
+  constructor(classs, str, dex, int) {
+    super(classs, str * 0.8, dex * 1.7, int * 2)
+  }
+}
+
+class Orc extends Person {
+  constructor(classs, str, dex, int) {
+    super(classs, str * 2, dex * 0.8, int * 0.2)
+  }
+}
+
+const Wizard = new Elf('Wizard', 10, 20, 70)
+
+const Warrior = new Orc('Warrior', 80, 10, 10)
+
+const Archer = new Human('Archer', 20, 60, 20)
+console.log(Archer)
+console.log(Warrior)
+console.log(Wizard)
